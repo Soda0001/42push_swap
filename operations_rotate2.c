4388652c1fef936dcb9d77015e40d1b18d3206fe
Soda0001 <selcukdeniz239@gmail.com>
2026-08-19 18:23:54 +0300

    op_r2.c & rr & rrr added

---
+++ operations_rotate2.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_rotate2.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sedeniz <sedeniz@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 18:20:21 by sedeniz           #+#    #+#             */
+/*   Updated: 2026/08/19 18:20:27 by sedeniz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	op_rr(t_node **head_a, t_node **head_b)
+{
+	op_ra(head_a);
+	op_rb(head_b);
+}
+
+void	op_rrr(t_node **head_a, t_node **head_b)
+{
+	op_rra(head_a);
+	op_rrb(head_b);
+}
