daab76befd1b6fc4bc6c511a40fa792b141bb573
Soda0001 <selcukdeniz239@gmail.com>
2026-08-21 18:14:33 +0300

    check_forced naming fix

--- ps_utils.c
+++ ps_utils.c
@@ -25,17 +25,17 @@ int	determine_start(char *first_arg)
 	return (starting_index);
 }
 
-t_strategy	check_forced(char *first_arg)
+t_strategy	check_forced(char *forced_command)
 {
-	if (ft_strncmp(first_arg, "--", 2) != 0)
+	if (ft_strncmp(forced_command, "--", 2) != 0)
 		return (STRAT_ADAPTIVE);
-	if (ft_strcmp(first_arg, "--simple") == 0)
+	if (ft_strcmp(forced_command, "--simple") == 0)
 		return (STRAT_SIMPLE);
-	if (ft_strcmp(first_arg, "--medium") == 0)
+	if (ft_strcmp(forced_command, "--medium") == 0)
 		return (STRAT_MEDIUM);
-	if (ft_strcmp(first_arg, "--complex") == 0)
+	if (ft_strcmp(forced_command, "--complex") == 0)
 		return (STRAT_COMPLEX);
-	if (ft_strcmp(first_arg, "--adaptive") == 0)
+	if (ft_strcmp(forced_command, "--adaptive") == 0)
 		return (STRAT_ADAPTIVE);
 	return (STRAT_INVALID);
 }
