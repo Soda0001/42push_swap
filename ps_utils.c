501f82f20dd9f26140a812117edde5f48ea23713
Soda0001 <selcukdeniz239@gmail.com>
2026-08-23 13:10:51 +0300

    parse_options implemented

--- ps_utils.c
+++ ps_utils.c
@@ -12,49 +12,9 @@
 
 #include "push_swap.h"
 
-void	foo()
+int	check_bench()
 {
-	int	i;
-	int	j;
-
 	
-	if (ft_strncmp(av[1], "--", 2) != 0)
-			return (STRAT_ADAPTIVE);
-	i = 1;
-	while ()
-	{
-		if (ft_strcmp(arg[i][j], command) == 0)
-
-	}
-}
-/*
-int	determine_start(char **av, int ac)
-{
-	int	i;
-
-	i = 1;
-	while (i < ac)
-	{
-		if (!ft_strncmp(av[i], "--", 2))
-			i++;
-		else
-			break ;
-	}
-	return (i);
-}
-*/
-
-int	determine_start(char *first_arg)
-{
-	int	starting_index;
-
-	starting_index = 1;
-	if (ft_strncmp(first_arg, "--", 2) == 0)
-	{
-		starting_index = 2;
-		return (starting_index);
-	}
-	return (starting_index);
 }
 
 t_strategy	check_forced(char *forced_command)
@@ -72,6 +32,46 @@ t_strategy	check_forced(char *forced_command)
 	return (STRAT_INVALID);
 }
 
+int	parse_options(char **av, int ac, t_strategy *strategy, int *bench)
+{
+	int	i;
+
+	i = 1;
+	*strategy = STRAT_ADAPTIVE;
+	*bench = 0;
+	if (i < ac && !ft_strncmp(av[i], "--", 2))
+	{
+		strategy = check_forced(av[i]);
+		if (!ft_strcmp(av[i], "--bench"))
+			*bench = 1;
+		else
+			return (-1);
+		i++;
+	}
+	if (i < ac && !ft_strcmp(av[i], "--bench"))
+	{
+		if (*bench)
+			return (-1);
+		*bench = 1;
+		i++;
+	}
+	return (i);
+}
+
+int	determine_start(char *first_arg)
+{
+	int	starting_index;
+
+	starting_index = 1;
+	if (ft_strncmp(first_arg, "--", 2) == 0)
+	{
+		starting_index = 2;
+		return (starting_index);
+	}
+	return (starting_index);
+}
+
+
 void	run_algo(t_strategy strategy, t_node **stack)
 {
 	if (strategy == STRAT_SIMPLE)
@@ -83,4 +83,3 @@ void	run_algo(t_strategy strategy, t_node **stack)
 	else if (strategy == STRAT_ADAPTIVE)
 		adaptive_sort(stack);
 }
-
